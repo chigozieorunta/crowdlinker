5ea09bd8eb8dec43d187596b1bcb489ccfad1e83
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-03-07 02:32:58 +0100

    Added theme header template file

---
+++ header.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Header.php
+ *
+ * @package Crowdlinker
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>">
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		<title>Crowdlinker</title>
+		<?php wp_head(); ?>
+	</head>
+	<body>
